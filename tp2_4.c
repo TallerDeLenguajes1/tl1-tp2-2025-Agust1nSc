4cd41a275a5f8799a2ea201e639d0e85f907957a
“Agust1nSc” <agustinsaccone9@gmail.com>
2025-04-05 13:21:41 -0300

    Funcion PC mas vieja

--- tp2_4.c
+++ tp2_4.c
@@ -11,6 +11,7 @@ struct compu
 };
 
 void listarPCs(struct compu PC[], int cantidad);
+void mostrarMasVieja(struct compu PC[], int cantidad);
 
 int main()
 {
@@ -20,7 +21,6 @@ int main()
     char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core",
                          "Pentium"};
 
-    
     int cantidad = 5;
 
     struct compu PC[cantidad];
@@ -32,21 +32,36 @@ int main()
         PC[i].cantidad_nucleos = 1 + rand() % (8);
 
         int indiceTipos = rand() % 6;
-        
+
         PC[i].tipo_cpu = tipos[indiceTipos];
-        
     }
 
     listarPCs(PC, cantidad);
+    mostrarMasVieja(PC, cantidad);
 
     return 0;
 }
 
-void listarPCs(struct compu PC[], int cantidad){
+void listarPCs(struct compu PC[], int cantidad)
+{
 
     for (int i = 0; i < cantidad; i++)
     {
-        printf("La pc numero, %d tiene velocidad de: %d, es del anio: %d, tiene %d nucleos y es del tipo: %s\n", i,PC[i].velocidad, PC[i].anio, PC[i].cantidad_nucleos, PC[i].tipo_cpu);
+        printf("La pc numero, %d tiene velocidad de: %d, es del anio: %d, tiene %d nucleos y es del tipo: %s\n", i, PC[i].velocidad, PC[i].anio, PC[i].cantidad_nucleos, PC[i].tipo_cpu);
     }
-    
+}
+
+void mostrarMasVieja(struct compu PC[], int cantidad)
+{
+
+    int PcMasVieja = 0;
+
+    for (int i = 0; i < cantidad; i++)
+    {
+        if (PC[i].anio < PC[PcMasVieja].anio)
+        {
+            PcMasVieja = i;
+        }
+    }
+    printf("La pc mas vieja es la, %d tiene velocidad de: %d, es del anio: %d, tiene %d nucleos y es del tipo: %s\n", PcMasVieja, PC[PcMasVieja].velocidad, PC[PcMasVieja].anio, PC[PcMasVieja].cantidad_nucleos, PC[PcMasVieja].tipo_cpu);
 }
